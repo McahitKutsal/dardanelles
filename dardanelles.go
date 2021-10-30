795c2a81080adeedcc8de14663385c374047fa27
Mucahit Kutsal <195541014@firat.edu.tr>
2021-10-31 01:02:14 +0300

    flag paketi yazıldı refactoring işlemleri yapıldı

--- dardanelles.go
+++ dardanelles.go
@@ -3,30 +3,43 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"main/flagvalue"
 	scanner "main/scan"
 
 	"golang.org/x/sync/semaphore"
 )
 
 var (
-	address string
-	portNum int
+	address    string
+	portNum    int
+	customFlag flagvalue.CustomFlag
 )
 
 func main() {
 
-	flag.StringVar(&address, "address", "", "")
-	flag.IntVar(&portNum, "port", 0, "")
+	flag.StringVar(&address, "address", "", "address to scan")
+	flag.IntVar(&portNum, "port", 0, "port for specified address")
 	flag.Parse()
+	stringInterval := flag.Args()
+
+	numberInterval, err := flagvalue.CheckInterval(stringInterval, portNum)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	customFlag = *customFlag.NewCustomFlag()
+	customFlag.SetAddress(address)
+	customFlag.SetPort(portNum)
+	customFlag.Interval.SetStart(numberInterval[0])
+	customFlag.Interval.SetEnd(numberInterval[1])
 
 	portScanner := &scanner.PortScanner{
-		Ip:     address,
+		Ip:     customFlag.GetAddress(),
 		Thread: semaphore.NewWeighted(5000),
 	}
-	if address == "" {
-		fmt.Println("Please specify an address with '--address' flag")
-	} else if portNum == 0 {
-		portScanner.Start()
+	if customFlag.GetPort() == 0 {
+		portScanner.Start(customFlag.Interval.GetStart(), customFlag.Interval.GetEnd())
 	} else {
 		scanResult := portScanner.ScanPort("tcp", address, portNum)
 		fmt.Println("address:", address, "port:", portNum, "is", scanResult.State)
